d2332c8f506710529c7f65065e78fa59397373d4
Jeferson Pimentel <jerfinho.2013@hotmail.com>
2021-03-22 21:15:04 -0300

    Pequenas alterações no codigo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,71 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { ScrollView, TextInput} from 'react-native-gesture-handler'
+import {Feather} from '@expo/vector-icons';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <ScrollView
+    showsVerticalScrollIndicator={false}
+    style={{backgroundColor: "#fff"}}
+    >
+
+    <View style={styles.header}>
+      <View style={styles.inputArea}>
+        <Feather name = "search" size ={24} color="#333" />
+        <TextInput
+        placeholder="O que está procurando?"  
+        style={styles.input}
+        />
+      </View>
     </View>
+
+    <View style={styles.contentNew}>
+      <Text style={styles.title}>
+        Novidades
+      </Text>
+    </View>
+
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+header:{
+  paddingHorizontal: 15,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  marginVertical: 20,
+},
+inputArea:{
+  paddingHorizontal: 15,
+  flexDirection: 'row',
+  alignItems: 'center',
+  width: '100%',
+  backgroundColor:"#fff",
+  elevation: 2,
+  paddingHorizontal: 10,
+  height: 37,
+  borderRadius:10,
+},
+input:{
+  fontFamily: 'Montserrat_500Medium',
+  paddingHorizontal: 10,
+  fontSize: 13,
+  width: '100%',
+},
+contentNew:{
+  flexDirection: 'row',
+  width:'100%',
+  alignItems: 'center',
+},
+title:{
+  paddingHorizontal: 15,
+  fontFamily:'Montserrat_700Bold',
+  fontSize: 18,
+  color:'#4f4a4a',
+},
+});
 
